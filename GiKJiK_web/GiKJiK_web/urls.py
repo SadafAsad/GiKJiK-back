1609c11c6973bbc7e2dc367c2ecbb00b049421aa
SadafAsad <sadaf98x@gmail.com>
2020-07-30 23:47:36 +0430

    urls organized

--- GiKJiK_web/GiKJiK_web/urls.py
+++ GiKJiK_web/GiKJiK_web/urls.py
@@ -20,28 +20,28 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    # auth
+    path('log-in/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('sign-up/', SignUpView.as_view(), name="sign_up"),
 
     # user
-    path('user/list/', UserProfileListView.as_view(), name="user_list"),
     path('user/<str:username>/search/', UserRetrieveView.as_view(), name="user_search"),
+
+    # class
+    path('class/create/', ClassCreateView.as_view(), name="class_create"),
+    path('class/<str:class_id>/search/', ClassRetrieveView.as_view(), name="class_search"),
+    path('class/<str:class_id>/join/', ClassJoinView.as_view(), name="class_join"),
+    path('class/<str:class_id>/remove/', ClassRemoveView.as_view(), name="class_remove"),
+    path('class/<str:class_id>/delete/', ClassDeleteView.as_view(), name="class_delete"),
+    path('class/<str:class_id>/change/status/', ClassChangeStatusView.as_view(), name="class_status_change"),
     path('user/student/in/', UserStudentInClassListView.as_view(), name="user_student_in"),
     path('user/teacher/in/', UserTeacherInClassListView.as_view(), name="user_teacher_in"),
     path('user/owner/of/', UserOwnerOfClassListView.as_view(), name="user_owner_of"),
 
     # teacher
     path('class/<str:class_id>/teacher/add-remove/', ClassAddRemoveTeacherView.as_view(), name="class_ar_teacher"),
-
-    # class
-    path('class/create/', ClassCreateView.as_view(), name="class_create"),
-    path('class/list/', ClassListView.as_view(), name="class_list"),
-    path('class/<str:class_id>/search/', ClassRetrieveView.as_view(), name="class_search"),
-    path('class/<str:class_id>/join/', ClassJoinView.as_view(), name="class_join"),
-    path('class/<str:class_id>/remove/', ClassRemoveView.as_view(), name="class_remove"),
-    path('class/<str:class_id>/delete/', ClassDeleteView.as_view(), name="class_delete"),
-    path('class/<str:class_id>/change/status/', ClassChangeStatusView.as_view(), name="class_status_change"),
 
     # student
     path('class/<str:class_id>/student/add-remove/', ClassAddRemoveStudentView.as_view(), name="student_add_remove"),
@@ -69,5 +69,7 @@ urlpatterns = [
     path('question/<int:question_id>/create/grade/', CreateGradeView.as_view(), name="create_grade"),
 
     # test
+    path('user/list/', UserProfileListView.as_view(), name="user_list"),
+    path('class/list/', ClassListView.as_view(), name="class_list"),
     path('test/', Test.as_view(), name="test"),
 ]
