03952fb087ee17d4a4044f3179f151d247e787c7
SadafAsad <sadaf98x@gmail.com>
2020-07-30 23:54:01 +0430

    views.py organized

--- GiKJiK_web/GiKJiK/views.py
+++ GiKJiK_web/GiKJiK/views.py
@@ -6,33 +6,14 @@ from GiKJiK.models import *
 from GiKJiK.serializers import *
 from GiKJiK.permissions import *
 
+# auth
 class SignUpView(generics.CreateAPIView):
     serializer_class = UserCreateSerializer
 
     def perform_create(self, serializer):
         serializer.save()
 
-class UserProfileListView(generics.ListAPIView):
-    serializer_class = UserProfileListSerializer
-    queryset = UserProfile.objects.all()
-
-class ClassCreateView(generics.CreateAPIView):
-    serializer_class = ClassCreateSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user.user_profile)
-
-class ClassListView(generics.ListAPIView):
-    serializer_class = ClassListSerializer
-    queryset = Class.objects.all()
-    
-class ClassRetrieveView(generics.RetrieveAPIView):
-    serializer_class = ClassListSerializer
-    permission_classes = [IsAuthenticated]
-    
-    def get_object(self):
-        return get_object_or_404(Class, class_id=self.kwargs.get('class_id'))
-
+# user
 class UserRetrieveView(generics.RetrieveAPIView):
     serializer_class = UserProfileListSerializer
     permission_classes = [IsAuthenticated]
@@ -41,19 +22,19 @@ class UserRetrieveView(generics.RetrieveAPIView):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
         return user.user_profile
 
-class ClassAddRemoveTeacherView(generics.UpdateAPIView):
-    permission_classes = (IsAuthenticated, IsClassOwner,)
-    queryset = Class.objects.all()
-    serializer_class = ClassAddRemoveTeacherSerializer
+# class
+class ClassCreateView(generics.CreateAPIView):
+    serializer_class = ClassCreateSerializer
 
-    lookup_field = 'class_id'
-    lookup_url_kwarg = 'class_id'
-
-    def perform_update(self, serializer):
-        if serializer.validated_data.get('action') == self.serializer_class.ADD:
-            serializer.instance.teachers.add(serializer.validated_data.get('teacher'))
-        else:
-            serializer.instance.teachers.remove(serializer.validated_data.get('teacher'))
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user.user_profile)
+    
+class ClassRetrieveView(generics.RetrieveAPIView):
+    serializer_class = ClassListSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_object(self):
+        return get_object_or_404(Class, class_id=self.kwargs.get('class_id'))
 
 class ClassJoinView(generics.UpdateAPIView):
     permission_classes = [IsAuthenticated]
@@ -101,67 +82,6 @@ class ClassRemoveView(generics.UpdateAPIView):
     def perform_update(self, serializer):
         serializer.instance.students.remove(self.request.user.user_profile)
 
-class NewsCreate(generics.CreateAPIView):
-    permission_classes = (IsAuthenticated, CanCreateNews, )
-    serializer_class = NewsCreateSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user.user_profile, _class=get_object_or_404(Class, class_id=self.kwargs.get('class_id')))
-
-class ClassNewsListView(generics.ListAPIView):
-    permission_classes = (IsAuthenticated, InClass, )
-    serializer_class = NewsListSerializer
-
-    def get_queryset(self):
-        t_class = get_object_or_404(Class, class_id=self.kwargs.get('class_id'))
-        return t_class.news.all()
-
-class ClassAddRemoveStudentView(generics.UpdateAPIView):
-    permission_classes = (IsAuthenticated, IsClassTeacher,)
-    queryset = Class.objects.all()
-    serializer_class = ClassAddRemoveStudentSerializer
-
-    lookup_field = 'class_id'
-    lookup_url_kwarg = 'class_id'
-
-    def perform_update(self, serializer):
-        if serializer.validated_data.get('action') == self.serializer_class.ADD:
-            serializer.instance.students.add(serializer.validated_data.get('student'))
-        else:
-            serializer.instance.students.remove(serializer.validated_data.get('student'))
-
-class QuizCreateView(generics.CreateAPIView):
-    permission_classes = (IsAuthenticated, IsClassTeacher, )
-    queryset = Class.objects.all()
-    serializer_class = QuizeCreateSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user.user_profile, _class=get_object_or_404(Class, class_id=self.kwargs.get('class_id')))
-
-class QuestionCreateView(generics.CreateAPIView):
-    permission_classes = (IsAuthenticated, CanEditQuiz, )
-    queryset = Quize.objects.all()
-    serializer_class = QuestionCreateSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(quize=get_object_or_404(Quize, pk=self.kwargs.get('quiz_id')))
-
-class ChoiceCreateView(generics.CreateAPIView):
-    permission_classes = (IsAuthenticated, CanEditQuestion, )
-    queryset = Question.objects.all()
-    serializer_class = ChoiceCreateSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(question=get_object_or_404(Question, pk=self.kwargs.get('question_id')))
-
-class QuizDeleteView(generics.DestroyAPIView):
-    permission_classes = (IsAuthenticated, CanEditQuiz, )
-    queryset = Quize.objects.all()
-    serializer_class = QuizeCreateSerializer
-
-    lookup_field = 'pk'
-    lookup_url_kwarg = 'quiz_id'
-
 class ClassDeleteView(generics.DestroyAPIView):
     permission_classes = (IsAuthenticated, IsClassOwner, )
     queryset = Class.objects.all()
@@ -182,6 +102,69 @@ class ClassChangeStatusView(generics.UpdateAPIView):
         serializer.instance.state = serializer.validated_data.get('state')
         serializer.save()
 
+# teacher
+class ClassAddRemoveTeacherView(generics.UpdateAPIView):
+    permission_classes = (IsAuthenticated, IsClassOwner,)
+    queryset = Class.objects.all()
+    serializer_class = ClassAddRemoveTeacherSerializer
+
+    lookup_field = 'class_id'
+    lookup_url_kwarg = 'class_id'
+
+    def perform_update(self, serializer):
+        if serializer.validated_data.get('action') == self.serializer_class.ADD:
+            serializer.instance.teachers.add(serializer.validated_data.get('teacher'))
+        else:
+            serializer.instance.teachers.remove(serializer.validated_data.get('teacher'))
+
+# student
+class ClassAddRemoveStudentView(generics.UpdateAPIView):
+    permission_classes = (IsAuthenticated, IsClassTeacher,)
+    queryset = Class.objects.all()
+    serializer_class = ClassAddRemoveStudentSerializer
+
+    lookup_field = 'class_id'
+    lookup_url_kwarg = 'class_id'
+
+    def perform_update(self, serializer):
+        if serializer.validated_data.get('action') == self.serializer_class.ADD:
+            serializer.instance.students.add(serializer.validated_data.get('student'))
+        else:
+            serializer.instance.students.remove(serializer.validated_data.get('student'))
+
+# news
+class NewsCreate(generics.CreateAPIView):
+    permission_classes = (IsAuthenticated, CanCreateNews, )
+    serializer_class = NewsCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user.user_profile, _class=get_object_or_404(Class, class_id=self.kwargs.get('class_id')))
+
+class ClassNewsListView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, InClass, )
+    serializer_class = NewsListSerializer
+
+    def get_queryset(self):
+        t_class = get_object_or_404(Class, class_id=self.kwargs.get('class_id'))
+        return t_class.news.all()
+
+# quiz
+class QuizCreateView(generics.CreateAPIView):
+    permission_classes = (IsAuthenticated, IsClassTeacher, )
+    queryset = Class.objects.all()
+    serializer_class = QuizeCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user.user_profile, _class=get_object_or_404(Class, class_id=self.kwargs.get('class_id')))
+
+class QuizDeleteView(generics.DestroyAPIView):
+    permission_classes = (IsAuthenticated, CanEditQuiz, )
+    queryset = Quize.objects.all()
+    serializer_class = QuizeCreateSerializer
+
+    lookup_field = 'pk'
+    lookup_url_kwarg = 'quiz_id'
+
 class ClassQuizListView(generics.ListAPIView):
     permission_classes = (IsAuthenticated, InClass, )
     serializer_class = QuizeCreateSerializer
@@ -189,6 +172,15 @@ class ClassQuizListView(generics.ListAPIView):
     def get_queryset(self):
         t_class = get_object_or_404(Class, class_id=self.kwargs.get('class_id'))
         return t_class.class_quizes.all()
+
+# question
+class QuestionCreateView(generics.CreateAPIView):
+    permission_classes = (IsAuthenticated, CanEditQuiz, )
+    queryset = Quize.objects.all()
+    serializer_class = QuestionCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(quize=get_object_or_404(Quize, pk=self.kwargs.get('quiz_id')))
 
 class QuizQuestionsView(generics.ListAPIView):
     permission_classes = (IsAuthenticated, IsClassTeacher_or_Student_by_quiz, )
@@ -198,6 +190,16 @@ class QuizQuestionsView(generics.ListAPIView):
         quiz = get_object_or_404(Quize, pk=self.kwargs.get('quiz_id'))
         return quiz.questions.all()
 
+# choice
+class ChoiceCreateView(generics.CreateAPIView):
+    permission_classes = (IsAuthenticated, CanEditQuestion, )
+    queryset = Question.objects.all()
+    serializer_class = ChoiceCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(question=get_object_or_404(Question, pk=self.kwargs.get('question_id')))
+
+# answer
 class CreateAnswerView(generics.CreateAPIView):
     permission_classes = (IsAuthenticated, IsClassStudent_by_question, )
     queryset = Question.objects.all()
@@ -206,10 +208,7 @@ class CreateAnswerView(generics.CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(question=get_object_or_404(Question, pk=self.kwargs.get('question_id')), student=self.request.user.user_profile)
 
-class Test(generics.ListAPIView):
-    serializer_class = QuizeCreateSerializer
-    queryset = Question.objects.all()
-
+# grade
 class CreateGradeView(generics.CreateAPIView):
     permission_classes = (IsAuthenticated, IsQuizauthor_by_question, )
     queryset = Question.objects.all()
@@ -227,3 +226,17 @@ class StudentQuizesGrades(APIView):
         return user.std_grades.all()
 
     # bayad betoonm grade aye ye quiz ro dast bandi konm jam bezanm va namayesh bedam ...
+
+
+# test
+class UserProfileListView(generics.ListAPIView):
+    serializer_class = UserProfileListSerializer
+    queryset = UserProfile.objects.all()
+
+class ClassListView(generics.ListAPIView):
+    serializer_class = ClassListSerializer
+    queryset = Class.objects.all()
+
+class Test(generics.ListAPIView):
+    serializer_class = QuizeCreateSerializer
+    queryset = Question.objects.all()
