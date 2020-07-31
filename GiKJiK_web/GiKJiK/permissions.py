c5830feb159689ad33bf968a7e8ff4600cd09ab2
SadafAsad <sadaf98x@gmail.com>
2020-07-31 18:04:07 +0430

    organizing

--- GiKJiK_web/GiKJiK/permissions.py
+++ GiKJiK_web/GiKJiK/permissions.py
@@ -6,68 +6,58 @@ class IsClassOwner(permissions.BasePermission):
 
     def has_permission(self, request, view):
         t_class = get_object_or_404(Class, class_id=view.kwargs.get('class_id'))
-        user = request.user
-        return t_class.owner == user.user_profile
+        return t_class.owner == request.user.user_profile
 
 class IsClassStudent(permissions.BasePermission):
 
     def has_permission(self, request, view):
         t_class = get_object_or_404(Class, class_id=view.kwargs.get('class_id'))
-        user = request.user
-        return t_class.students.filter(django_user=user).exists()
+        return t_class.students.filter(django_user=request.user).exists()
 
 class CanCreateNews(permissions.BasePermission):
 
     def has_permission(self, request, view):
         t_class = get_object_or_404(Class, class_id=view.kwargs.get('class_id'))
-        user = request.user
-        return (t_class.owner == user.user_profile) or (t_class.teachers.filter(django_user=user).exists())
+        return (t_class.owner == request.user.user_profile) or (t_class.teachers.filter(django_user=request.user).exists())
 
 class InClass(permissions.BasePermission):
 
     def has_permission(self, request, view):
         t_class = get_object_or_404(Class, class_id=view.kwargs.get('class_id'))
-        user = request.user
-        return (t_class.owner == user.user_profile) or (t_class.teachers.filter(django_user=user).exists()) or (t_class.students.filter(django_user=user).exists())
+        return (t_class.owner == request.user.user_profile) or (t_class.teachers.filter(django_user=request.user).exists()) or (t_class.students.filter(django_user=request.user).exists())
 
 class IsClassTeacher(permissions.BasePermission):
 
     def has_permission(self, request, view):
         t_class = get_object_or_404(Class, class_id=view.kwargs.get('class_id'))
-        user = request.user
-        return t_class.teachers.filter(django_user=user).exists()
+        return t_class.teachers.filter(django_user=request.user).exists()
 
 class CanEditQuiz(permissions.BasePermission):
 
     def has_permission(self, request, view):
         quiz = get_object_or_404(Quize, pk=view.kwargs.get('quiz_id'))
-        user = request.user.user_profile
-        return quiz.author == user
+        return quiz.author == request.user.user_profile
 
 class CanEditQuestion(permissions.BasePermission):
 
     def has_permission(self, request, view):
         question = get_object_or_404(Question, pk=view.kwargs.get('question_id'))
-        user = request.user.user_profile
-        return question.quize.author == user
+        return question.quize.author == request.user.user_profile
 
 class IsClassTeacher_or_Student_by_quiz(permissions.BasePermission):
 
     def has_permission(self, request, view):
         quiz = get_object_or_404(Quize, pk=view.kwargs.get('quiz_id'))
-        user = request.user
-        return (quiz.author == user.user_profile) or (quiz._class.students.filter(django_user=user).exists())
+        return (quiz.author == request.user.user_profile) or (quiz._class.students.filter(django_user=request.user).exists())
         
 class IsClassStudent_by_question(permissions.BasePermission):
 
     def has_permission(self, request, view):
         question = get_object_or_404(Question, pk=view.kwargs.get('question_id'))
-        user = request.user
-        return question.quize._class.students.filter(django_user=user).exists()
+        return question.quize._class.students.filter(django_user=request.user).exists()
 
 class IsQuizauthor_by_question(permissions.BasePermission):
 
     def has_permission(self, request, view):
         question = get_object_or_404(Question, pk=view.kwargs.get('question_id'))
-        user = request.user.user_profile
-        return question.quize.author == user
+        return question.quize.author == request.user.user_profile
